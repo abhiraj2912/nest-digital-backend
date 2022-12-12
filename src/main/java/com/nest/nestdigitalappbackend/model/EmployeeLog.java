2b1a49d94691da17cbab53f76fdb1f69949f4dec
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-12 12:52:21 +0530

    entity

---
+++ src/main/java/com/nest/nestdigitalappbackend/model/EmployeeLog.java
@@ -0,0 +1,67 @@
+package com.nest.nestdigitalappbackend.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class EmployeeLog {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private int empId;
+    private String date;
+    private String entryTime;
+    private String exitTime;
+
+
+    public EmployeeLog() {
+    }
+
+    public EmployeeLog(int id, int empId, String date, String entryTime, String exitTime) {
+        this.id = id;
+        this.empId = empId;
+        this.date = date;
+        this.entryTime = entryTime;
+        this.exitTime = exitTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(String entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public String getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime(String exitTime) {
+        this.exitTime = exitTime;
+    }
+}
