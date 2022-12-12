7fd53079addcc6838a1cad8a28e99a8841e2768b
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-12 12:06:33 +0530

    first commit

---
+++ src/main/java/com/nest/nestdigitalappbackend/NestdigitalappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestdigitalappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestdigitalappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nestdigitalappbackend/NestdigitalappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
